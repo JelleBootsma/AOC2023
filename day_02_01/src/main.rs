7122aedeed4c5dbe6fc6a1837fb280b10750ed50
Jelle Bootsma <jellehb@gmail.com>
2023-12-06 19:57:37 +0100

    WIP on day 2

--- day_02_01/src/main.rs
+++ day_02_01/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt")
+        .expect("Should have been able to read this file without issues");
+
+    let games: std::str::Split<'_, char> = input.split('\n');
+    for game in games {
+        println!("Hello world!")
+    }
 }
